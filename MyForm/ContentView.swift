5de143eb507eb7a620146fdc828e4ede2f3975a0
karhoshe <karhoshe@me.com>
2021-05-06 00:00:16 +0800

    First try of MyForm

--- MyForm/ContentView.swift
+++ MyForm/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var birthdate = Date()
+    @State private var shouldSendNewsletter = false
+    @State private var numberOfLikes = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form {
+                Section(header: Text("Personal Information")){
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    DatePicker("Birthdate", selection: $birthdate, displayedComponents: .date)
+                }
+                
+                Section(header: Text("Action")){
+                    Toggle("Send Newsletter", isOn: $shouldSendNewsletter)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of likes", value: $numberOfLikes, in: 1...100)
+                    Text("This video has \(numberOfLikes) likes")
+                    Link("Terms of Service", destination: URL(string: "https://www.youtube.com/channel/UClGp-XpBWjDOLKSi2Xyi2og")!)
+                }
+
+            }
+            .accentColor(.red)
+            .navigationTitle("Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing){
+                    Button{
+                        
+                    } label:{
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                }
+            }
+        }
     }
 }
 
