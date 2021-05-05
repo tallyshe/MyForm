25375a74968d95aa3d28913bf5571c9337f6cb65
karhoshe <karhoshe@me.com>
2021-05-06 00:16:28 +0800

    import UIKit
    adjust the .accentColor to NavigationView
    
    add another Button for saveUser

--- MyForm/ContentView.swift
+++ MyForm/ContentView.swift
@@ -33,18 +33,25 @@ struct ContentView: View {
                 }
 
             }
-            .accentColor(.red)
+            
             .navigationTitle("Account")
             .toolbar {
                 ToolbarItemGroup(placement: .navigationBarTrailing){
                     Button{
-                        
+                        hideKeyboard()
                     } label:{
                         Image(systemName: "keyboard.chevron.compact.down")
                     }
+                    
+                    Button("Save", action: saveUser)
                 }
             }
         }
+        .accentColor(.red)
+    }
+    
+    func saveUser() {
+        print("User Saved")
     }
 }
 
@@ -53,3 +60,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
