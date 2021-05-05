bb322b6c2507845a1c362dd472b5ed7f97996461
karhoshe <karhoshe@me.com>
2021-05-05 22:54:50 +0800

    Initial Commit

---
+++ MyForm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyForm
+//
+//  Created by Kar Ho She on 5/5/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyForm/MyFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFormApp.swift
+//  MyForm
+//
+//  Created by Kar Ho She on 5/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
